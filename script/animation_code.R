f37efc6cf4a551d82399e6b7e15f2e37e1c447dd
Myfanwy <myfanwy@github.com>
2020-06-19 15:20:22 -0700

    refactored animation_code

--- script/animation_code.R
+++ script/animation_code.R
@@ -1,50 +1,18 @@
 # Thu Jun 18 10:57:04 2020 ------------------------------
-
+source("script/utils.R")
 # setup -------------------------------------------------------------------
-# make list of required packages
-list.of.packages <- c("tidyverse",
-                      "lubridate",
-                      "maps", 
-                      "rgeos",
-                      "maptools",
-                      "ggmap",
-                      "gganimate", 
-                      "sf", 
-                      "rgdal"
-)
+check_and_install_packages()
+download_telemetry_data()
 
-# make list of packages that are required, but not already downloaded
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
-
-# download packages that are not already present in the library
-if(length(new.packages))
-  install.packages(new.packages)
-
-# load packages
-packages_load <- lapply(list.of.packages, require, character.only = T)
-
-# print warning if there is a problem with installing/loading some of packages
-if(any(as.numeric(packages_load)==0)){
-  warning(paste("Package/s: ", paste(list.of.packages[packages_load != T], sep = ","), "not loaded!"))
-}else{
-  print("All packages were successfully loaded.")
-}
-
-
-# get data ----------------------------------------------------------------
-# set up temporary file
-tempdl <- tempfile()
-
-# download zipped data file from BARD website
-download.file('http://cftc.metro.ucdavis.edu/biotelemetry-autonomous-real-time-database/resources/csvs/UCD_GS_RESCUE.zip', 
-              tempdl, 
-              mode = 'wb')
-
-# unzip into data folder
-unzip(tempdl, "UCD_GS_RESCUE.csv", exdir = "data")
 
 # read csv into work space (also reducing the # of TagIDs read to simplify)
-gs_dat <- read_csv_chunked('data/UCD_GS_RESCUE.csv', callback = DataFrameCallback$new(function(x, pos) subset(x, TagID>46635)),  progress = T)
+gs_dat <-
+  read_csv_chunked(
+    'data/UCD_GS_RESCUE.csv',
+    callback = DataFrameCallback$new(function(x, pos)
+      subset(x, TagID > 46635)),
+    progress = TRUE
+  )
 
 ## Prepare the data
 
@@ -84,46 +52,73 @@ GS46638 <- dets %>%
 # Now plot a single fish
 # Following the tutorial found [here](https://medium.com/@mueller.johannes.j/use-r-and-gganimate-to-make-an-animated-map-of-european-students-and-their-year-abroad-517ad75dca06).
 
-CA_map<- map_data('state', region = 'california') # get outline of CA
+CA_map <-
+  map_data('state', region = 'california') # get outline of CA
 
-CA<-c(geom_polygon(aes(long,lat,group=group), 
-                   size = 0.1, 
-                   colour= "#090D2A",#090D2A", 
-                   fill="#090D2A", alpha=0.8, data=CA_map))
+CA <- c(
+  geom_polygon(
+    aes(long, lat, group = group),
+    size = 0.1,
+    colour = "#090D2A",
+    #090D2A",
+    fill = "#090D2A",
+    alpha = 0.8,
+    data = CA_map
+  )
+)
 
 
 plot <- ggplot() +
   CA +                                                                               # ad CA outline to plot
   geom_point(data = recs_short, aes(x = lon, y = lat), color = "white") +            # add receiver array to plot
-  geom_point(data = GS46638, aes(x = Lon, y = Lat), color = "red", size = 3.5) + 
-  annotate("text", label = paste("TagID:",GS46638$TagID[1]),                         # add text label to plot
-           color =  "white", x = -125, y = 37.1, size = 6) +
-  theme(panel.background =  element_rect(fill='gray40',colour='gray40'),
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank()) + 
-  coord_cartesian(xlim = c(-125.5, -119), ylim = c(37,41)) +
-  theme(axis.line=element_blank(),axis.text.x=element_blank(),
-        axis.text.y=element_blank(),axis.ticks=element_blank(),
-        axis.title.x=element_blank(),
-        axis.title.y=element_blank()) + 
-  transition_manual(frames = DetectDate) + 
+  geom_point(data = GS46638,
+             aes(x = Lon, y = Lat),
+             color = "red",
+             size = 3.5) +
+  annotate(
+    "text",
+    label = paste("TagID:", GS46638$TagID[1]),
+    # add text label to plot
+    color =  "white",
+    x = -125,
+    y = 37.1,
+    size = 6
+  ) +
+  theme(
+    panel.background =  element_rect(fill = 'gray40', colour = 'gray40'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  ) +
+  coord_cartesian(xlim = c(-125.5,-119), ylim = c(37, 41)) +
+  theme(
+    axis.line = element_blank(),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank()
+  ) +
+  transition_manual(frames = DetectDate) +
   labs(title = 'Location: {GS46638$DetectionLocation[current_frame]}',
        subtitle = 'Date: {current_frame}')
 
-# animate plot
-animate(plot, fps=2)
+
 
 # create fig_output directory
 dir.create("fig_output")
 
+if(FALSE){ # none of the code between these braces worked to its intended end for me; see comments below. Don't have time to troubleshoot unfortunately, but consider simplifying
+
+animate(plot, fps = 2) # animate plot - not run; creates a bunch of .pngs in root directory
+
 # save animation to directory
-anim_save("fig_output/GS46638.gif", plot)
+anim_save("fig_output/GS46638.gif", plot, device = "png") # Error: The animation object does not specify a save_animation method            
 
 # optional: save as mp4 to pause and click through frame by frame
-b <- animate(plot, renderer = av_renderer())
+b <- animate(plot, renderer = av_renderer()) # Error: The av package is required to use av_renderer; add to your package list or remove dependency here
 anim_save("fig_output/GS46638.mp4", b)
 
-
+}
 
 
 # Faceted plot ------------------------------------------------------------
@@ -131,7 +126,6 @@ anim_save("fig_output/GS46638.mp4", b)
 # subset data by time  - keep post rescue detections; not entire detection history
 rescue_year <- dets %>% 
   filter(DetectDate<= "2012-01-01 00:00:00")
-
 
 
 plot2 <- ggplot() +
@@ -153,5 +147,7 @@ plot2 <- ggplot() +
   transition_time(DetectDate) + 
   labs(title = "Date: {frame_time}")
 
+if(FALSE) { # same animation issues as above
 # animate plot
 animate(plot2, fps=2, renderer = gifski_renderer(loop = F))
+}
